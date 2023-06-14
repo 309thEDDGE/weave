27968552fcbc581f03381ea0a77a769e17c7858c
AB-USAF <alexander.bedoya@us.af.mil>
2023-06-14 22:32:23 +0000

    added some functionality to validate, and some notes in test_validate

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,3 +1,9 @@
 #in here we test everything we want to try and break it with
 #every test function can only have 1 assert statement
 #
+
+from fsspec.implementations.local import LocalFileSystem
+import s3fs
+import pytest
+from weave import validate
+
--- weave/validate.py
+++ weave/validate.py
@@ -2,8 +2,66 @@
 #basket_data
 #take in the bucket name as the argument
 
+import os
 import json
 from jsonschema import validate
 
+from weave import config
+from fsspec.implementations.local import LocalFileSystem
+import s3fs
+
+
+
 def validate_bucket(bucket_name):
+    # the bucket name would be something like this: 
+    # 7b71a3f0654093511babbeeba3d0242c0a82011
+    # this is like a folder name, then there should be:
+    # manifest, supplement, and sometimes the metadata. 
+    # if we do have the metadata, we need to make sure we can read it into a json object
+    # my current working notes too:
+    # bucket_name/basket_type/unique_id
+    
+    basket_address = os.fspath(bucket_name)
+    manifest_path = f"{basket_address}/basket_manifest.json"
+    supplement_path = f"{basket_address}/basket_supplement.json"
+    metadata_path = f"{basket_address}/basket_metadata.json"
+    
+    
+    
+    
+    if os.path.isfile(manifest_path):
+        print('manifest is a real file')
+        f = open(manifest_path)
+        
+        data = json.load(f)
+        # print(data)
+        
+        validate(instance=data, schema=config.manifest_schema)
+        
+    
+    if os.path.isfile(supplement_path):
+        print('supplement is a real file')        
+        f = open(supplement_path)
+        
+        data = json.load(f)
+        # print(data)
+        
+        validate(instance=data, schema=config.supplement_schema)
+        
+        
+              
+    if os.path.isfile(metadata_path):
+        print('metadata is a real file')        
+        f = open(metadata_path)
+        
+        data = json.load(f)
+        # print(data)
+        
+        
+        
+
+        
+tempPath = './TestingValidation/'
+validate_bucket(tempPath)
+   
     
