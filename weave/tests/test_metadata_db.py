5e23cfe482c98b7d373052f6999804f0f37cacbb
Jonathan Adamic <jonathan.adamic@us.af.mil>
2023-07-10 19:43:16 +0000

    temp

--- weave/tests/test_metadata_db.py
+++ weave/tests/test_metadata_db.py
@@ -8,8 +8,7 @@ import weave
 
 class TestMongo():
     
-    @patch("weave.config.get_file_system", return_value=LocalFileSystem())
-    def load_data(self, patch):
+    def load_data(self):
         weave.uploader.upload_basket(
             [{"path": self.local_dir_path, "stub": False}],
             f"{self.bucket_path}/{self.basket_type}/1234",
@@ -64,8 +63,7 @@ class TestMongo():
             
         self.mongodb[self.test_collection].drop()
     
-    @patch("weave.config.get_file_system", return_value=LocalFileSystem())
-    def test_load_mongo(self, patch1):
+    def test_load_mongo(self):
         index_table = weave.index.create_index_from_s3(self.bucket_path)
         weave.load_mongo(index_table, self.test_collection)
         truth_db = [{'uuid': '1234', 'basket_type': 'test_basket_type', 
@@ -123,35 +121,26 @@ class TestMongo():
                              self.test_collection)
             
     def test_load_mongo_check_for_duplicate_uuid(self):
-        test_uuid = '42069'
-        
-        # Ensure uuid doesn't exist already.
-        count = self.mongodb[self.test_collection].count_documents(
-            {'uuid': test_uuid})
-        assert count == 0, "test uuid already exists"
+        test_uuid = '1234'
         
         # Create the test basket.
-        weave.uploader.upload_basket(
-            [{"path": self.local_dir_path, "stub": False}],
-            f"{self.bucket_path}/{self.basket_type}/{test_uuid}",
-            test_uuid,
-            self.basket_type,
-            metadata={'key1': 'value1'}
-        )
+        # weave.uploader.upload_basket(
+        #     [{"path": self.local_dir_path, "stub": False}],
+        #     f"{self.bucket_path}/{self.basket_type}/{test_uuid}",
+        #     test_uuid,
+        #     self.basket_type,
+        #     metadata={'key1': 'value1'}
+        # )
         
-        test_metadata = pd.DataFrame(
-            {'uuid': [test_uuid],
-            'address': [f"{self.bucket_path}/{self.basket_type}/{test_uuid}"],
-            'basket_type': ['type']})
+        # test_metadata = pd.DataFrame(
+        #     {'uuid': [test_uuid],
+        #     'address': [f"{self.bucket_path}/{self.basket_type}/{test_uuid}"],
+        #     'basket_type': ['type']})
         
-        # Load the metadata to the mongoDB
-        weave.load_mongo(test_metadata, self.test_collection)
-        count = self.mongodb[self.test_collection].count_documents(
-            {'uuid': test_uuid})
-        assert count == 1, "insert failed"
-        
-        # Attempt to load it again, and ensure it didn't
-        weave.load_mongo(test_metadata, self.test_collection)
+        # Load metadata again, and ensure there's still only one instance
+        index_table = weave.index.create_index_from_s3(self.bucket_path)
+        weave.load_mongo(index_table, self.test_collection)
+        weave.load_mongo(index_table, self.test_collection)
         count = self.mongodb[self.test_collection].count_documents(
             {'uuid': test_uuid})
         assert count == 1, "duplicate uuid inserted"
