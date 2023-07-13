75a64b89a3f60f959e9e70b18423f4123fbfb4f6
Jonathan Adamic <jonathan.adamic@us.af.mil>
2023-07-13 19:24:58 +0000

    Fixed test_access... I think?

--- weave/tests/test_access.py
+++ weave/tests/test_access.py
@@ -2,87 +2,100 @@ import os
 import tempfile
 import pytest
 import uuid
-from fsspec.implementations.local import LocalFileSystem
-from unittest.mock import patch
 
-from weave import upload
-
+from weave import upload, config
 
 class TestUpload:
-    def setup_class(self):
-        self.fs = LocalFileSystem()
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir        
+        self.s3fs_client = config.get_file_system()
         self.basket_type = "test_basket_type"
-        self.bucket_name = "weave-test-bucket"
+        self.test_bucket = "test-bucket"
+        self._set_up_bucket()
+        
+        self.local_dir_path = self.tmpdir.mkdir("test_dir_local")
+        print("local_dir_path", self.local_dir_path)
+        self.tmp_basket_txt_file = self.local_dir_path.join("test.txt")
+        self.tmp_basket_txt_file.write("This is a file for testing purposes")
+        
+        self.upload_items = [{"path": self.tmp_basket_txt_file.strpath,
+                              "stub": False}]
+        self.run_uploader()
 
-        self.file_system_dir = tempfile.TemporaryDirectory()
-        self.file_system_dir_path = self.file_system_dir.name
+    def _set_up_bucket(self):
+        try:
+            self.s3fs_client.mkdir(self.test_bucket)
+        except FileExistsError:
+            self.cleanup_bucket()
+            self._set_up_bucket()
+        
+    def cleanup_bucket(self):
+        self.s3fs_client.rm(self.test_bucket, recursive=True)
 
-        # make sure minio bucket doesn't exist. if it does, delete it.
-        if self.fs.exists(f"{self.bucket_name}"):
-            self.fs.rm(f"{self.bucket_name}", recursive=True)
-
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self.local_dir_path = self.temp_dir.name
-        self.data_file_path = os.path.join(self.local_dir_path, "test.txt")
-        with open(self.data_file_path, "w") as f:
-            f.write("0123456789")
-
-        self.upload_items = [{"path": self.data_file_path, "stub": False}]
-
-    def teardown_class(self):
-        self.temp_dir.cleanup()
-        if self.fs.exists(f"{self.bucket_name}"):
-            self.fs.rm(f"{self.bucket_name}", recursive=True)
-
-        self.file_system_dir.cleanup()
-
-    @pytest.fixture
-    @patch("weave.config.get_file_system", return_value=LocalFileSystem())
-    def run_uploader(self, patch):
+    def run_uploader(self):
         parent_ids = [uuid.uuid1().hex]
         metadata = {"oh": "i don't know", "something": "stupid"}
         label = "my label"
         self.upload_path = upload(
             self.upload_items,
             self.basket_type,
-            self.bucket_name,
+            self.test_bucket,
             parent_ids,
             metadata,
             label,
-            test_prefix=self.file_system_dir_path,
+            test_prefix=self.local_dir_path,
         )
+        
+        self.uploaded_files = self.s3fs_client.ls(self.upload_path)
 
-        uploaded_files = self.fs.ls(self.upload_path)
-        return uploaded_files
+@pytest.fixture
+def set_up(tmpdir):
+    tu = TestUpload(tmpdir)
+    yield tu
+    tu.cleanup_bucket()
+    
+def test_upload_test_txt_in_uploaded_files(set_up):
+    tu = set_up
+    
+    # print(f"\nUpload Path: {tu.upload_path}")
+    # print(f"Upload File {os.path.join(tu.upload_path, 'test.txt').lstrip(os.sep)}")
+    # print("Uploaded Files", tu.uploaded_files)
+    
+    assert (
+        os.path.join(tu.upload_path, "test.txt").lstrip(os.sep) 
+        in tu.uploaded_files
+    )
 
-    def test_upload_test_txt_in_uploaded_files(self, run_uploader):
-        assert os.path.join(self.upload_path, "test.txt") in run_uploader
+def test_upload_basket_manifest_in_uploaded_files(set_up):
+    tu = set_up
+    assert (
+        os.path.join(tu.upload_path, "basket_manifest.json").lstrip(os.sep)
+        in tu.uploaded_files
+    )
 
-    def test_upload_basket_manifest_in_uploaded_files(self, run_uploader):
-        assert (
-            os.path.join(self.upload_path, "basket_manifest.json")
-            in run_uploader
-        )
+def test_upload_basket_supplement_in_uploaded_files(set_up):
+    tu = set_up
+    assert (
+        os.path.join(tu.upload_path, "basket_supplement.json").lstrip(os.sep)
+        in tu.uploaded_files
+    )
 
-    def test_upload_basket_supplement_in_uploaded_files(self, run_uploader):
-        assert (
-            os.path.join(self.upload_path, "basket_supplement.json")
-            in run_uploader
-        )
+def test_upload_basket_metadata_in_uploaded_files(set_up):
+    tu = set_up
+    assert (
+        os.path.join(tu.upload_path, "basket_metadata.json").lstrip(os.sep)
+        in tu.uploaded_files
+    )
 
-    def test_upload_basket_metadata_in_uploaded_files(self, run_uploader):
-        assert (
-            os.path.join(self.upload_path, "basket_metadata.json")
-            in run_uploader
-        )
+def test_upload_nothing_else_in_uploaded_files(set_up):
+    tu = set_up
+    assert len(tu.uploaded_files) == 4
 
-    def test_upload_nothing_else_in_uploaded_files(self, run_uploader):
-        assert len(run_uploader) == 4
+def test_upload_bucket_name_is_string(set_up):
+    tu = set_up
+    bucket_name = 7
 
-    def test_upload_bucket_name_is_string(self):
-        bucket_name = 7
-
-        with pytest.raises(
-            TypeError, match=f"'bucket_name' must be a string: '{bucket_name}'"
-        ):
-            upload(self.upload_items, self.basket_type, bucket_name)
+    with pytest.raises(
+        TypeError, match=f"'bucket_name' must be a string: '{bucket_name}'"
+    ):
+        upload(tu.upload_items, tu.basket_type, bucket_name)
