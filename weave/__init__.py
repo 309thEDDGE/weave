518c0bd66c14d12427f12b1899849d7e32d59784
Douglas Fenwick <douglas.fenwick@us.af.mil>
2023-08-02 19:05:20 +0000

    Module level docstring added

--- weave/__init__.py
+++ weave/__init__.py
@@ -1,3 +1,24 @@
+"""
+Weave
+=====
+
+Weave can be used to facilitate the creation, maintenance, and access of
+coordinated, complex data storage.
+
+Weave is used to create Baskets. A Basket is used to store an atomic unit (i.e.
+the smallest sensible unit) of data, as well as it's associated metadata,
+including lineage.
+
+--> Note that there is a Basket class available in Weave. Use it to access
+individual Baskets.
+
+A collection of Baskets is known as a Pantry. A Pantry can be accessed using
+it's Index.
+
+--> Note that you can use the Index class to access a Pantry's Index. You can
+also upload new Baskets to the Pantry using Index.upload_basket().
+"""
+
 from .uploader import upload
 from .index import Index
 from .basket import Basket
