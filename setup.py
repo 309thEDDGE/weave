a8df416f9629cf8fb1137a78ea88088213e22bd6
Douglas Fenwick <douglas.fenwick@us.af.mil>
2023-08-02 19:07:26 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="weave",
-    version="0.5.2",
+    version="0.6.0",
     packages=["weave", "weave/tests"],
     install_requires=["pandas", "s3fs", "fsspec", "pymongo", "jsonschema"]
 )
