8755e55f7b692e03a97466b0b89a2f003402f416
Douglas Fenwick <douglas.fenwick@us.af.mil>
2023-08-11 00:33:33 +0000

    Simplification of logic in _validate_basket(), Pylint

--- weave/validate.py
+++ weave/validate.py
@@ -1,3 +1,5 @@
+"""Contains functions and classes used by uploader.py's upload function.
+"""
 import os
 import json
 import jsonschema
@@ -22,17 +24,14 @@ def validate_bucket(bucket_name, file_system):
     ----------
     A bool of whether the bucket is valid or not that comes from check_level()
     """
-
-    fs = file_system
-
-    if not fs.exists(bucket_name):
+    if not file_system.exists(bucket_name):
         raise ValueError(
             f"Invalid Bucket Path. Bucket does not exist at: {bucket_name}"
         )
 
     # call check level, with a path, but since we're just starting,
     # we just use the bucket_name as the path
-    return _check_level(bucket_name, fs)
+    return _check_level(bucket_name, file_system)
 
 
 def _check_level(current_dir, file_system, in_basket=False):
@@ -61,10 +60,7 @@ def _check_level(current_dir, file_system, in_basket=False):
         a true if we found a manifest while inside another basket
         a default true if no basket is found
     """
-
-    fs = file_system
-
-    if not fs.exists(current_dir):
+    if not file_system.exists(current_dir):
         raise ValueError(
             f"Invalid Path. No file or directory found at: {current_dir}"
         )
@@ -72,37 +68,38 @@ def _check_level(current_dir, file_system, in_basket=False):
     manifest_path = os.path.join(current_dir, 'basket_manifest.json')
 
     # if a manifest exists, its a basket, validate it
-    if fs.exists(manifest_path):
+    if file_system.exists(manifest_path):
         # if we find another manifest inside a basket, we just need to say
         # we found it, we don't need to validate the nested basket
         if in_basket:
             return True
-        return _validate_basket(current_dir, fs)
+        return _validate_basket(current_dir, file_system)
 
     # go through all the other files, if it's a directory, we need to check it
-    dirs_and_files = fs.find(
+    dirs_and_files = file_system.find(
         path=current_dir,
         maxdepth=1,
         withdirs=True
     )
 
     for file_or_dir in dirs_and_files:
-        file_type = fs.info(file_or_dir)['type']
+        file_type = file_system.info(file_or_dir)['type']
 
         if file_type == 'directory':
             # if we are in a basket, check evrything under it, for a manifest
             # and return true, this will return true to the _validate_basket
             # and throw an error
             if in_basket:
-                return _check_level(file_or_dir, fs, in_basket=in_basket)
+                return _check_level(file_or_dir, file_system,
+                                    in_basket=in_basket)
             # if we aren't in the basket, we want to check all files in our
             # current dir. If everything is valid, _check_level returns true
             # if it isn't valid, we go in and return false
             # we don't want to return _check_level because we want to keep
             # looking at all the sub-directories
-            else:
-                if not _check_level(file_or_dir, fs, in_basket=in_basket):
-                    return False
+            if not _check_level(file_or_dir, file_system,
+                                  in_basket=in_basket):
+                return False
 
     # This is the default backup return.
     # If we are in a basket, it will be valid if we return false,
@@ -132,95 +129,158 @@ def _validate_basket(basket_dir, file_system):
     Parameters
     ----------
     basket_dir: string
-        the path in s3fs to the basket root directory
+        The path in the file system to the basket root directory
+    file_system: fsspec object
+        The fsspec file system hosting the bucket to be indexed.
 
     Returns
-    ----------
+    -------
     boolean that is true when the Basket is valid
         if the Basket is invalid, raise an error
     """
-
-    fs = file_system
-
     manifest_path = os.path.join(basket_dir, 'basket_manifest.json')
     supplement_path = os.path.join(basket_dir, 'basket_supplement.json')
 
     # a valid basket has both manifest and supplement
     # if for some reason the manifest is gone, we get a wrong directory,
-    # or this function is incorrectly called, 
-    # we can say that this isn't a basket. 
-    if not fs.exists(manifest_path):
+    # or this function is incorrectly called,
+    # we can say that this isn't a basket.
+    if not file_system.exists(manifest_path):
         raise FileNotFoundError(
             f"Invalid Path. No Basket found at: {basket_dir}"
         )
 
-    if not fs.exists(supplement_path):
+    if not file_system.exists(supplement_path):
         raise FileNotFoundError(
             "Invalid Basket. No Supplement file found at: ", basket_dir
         )
 
-    files_in_basket = fs.find(
+    files_in_basket = file_system.find(
         path=basket_dir,
         maxdepth=1,
         withdirs=True
     )
 
     for file in files_in_basket:
-        basket_dir, file_name = os.path.split(file)
+        _, file_name = os.path.split(file)
+        {
+            "basket_manifest.json": handle_manifest,
+            "basket_supplement.json": handle_supplement,
+            "basket_metadata.json": handle_metadata
+        }.get(file_name, handle_none_of_the_above)(file, file_system)
+#         if file_name == 'basket_manifest.json':
+#             try:
+#                 # these two lines make sure it can be read and is valid schema
+#                 data = json.load(file_system.open(file))
+#                 validate(instance=data, schema=config.manifest_schema)
 
-        if file_name == 'basket_manifest.json':
-            try:
-                # these two lines make sure it can be read and is valid schema
-                data = json.load(fs.open(file))
-                validate(instance=data, schema=config.manifest_schema)
+#             except jsonschema.exceptions.ValidationError as exc:
+#                 raise ValueError(
+#                     "Invalid Basket. Manifest Schema does not match at: ", file
+#                 ) from exc
 
-            except jsonschema.exceptions.ValidationError:
-                raise ValueError(
-                    "Invalid Basket. Manifest Schema does not match at: ", file
-                )
+#             except json.decoder.JSONDecodeError as exc:
+#                 raise ValueError(
+#                     "Invalid Basket. "
+#                     "Manifest could not be loaded into json at: ", file
+#                 ) from exc
 
-            except json.decoder.JSONDecodeError:
-                raise ValueError(
-                    "Invalid Basket. "
-                    "Manifest could not be loaded into json at: ", file
-                )
+#         if file_name == 'basket_supplement.json':
+#             try:
+#                 # these two lines make sure it can be read and is valid schema
+#                 data = json.load(file_system.open(file))
+#                 validate(instance=data, schema=config.supplement_schema)
 
-        if file_name == 'basket_supplement.json':
-            try:
-                # these two lines make sure it can be read and is valid schema
-                data = json.load(fs.open(file))
-                validate(instance=data, schema=config.supplement_schema)
+#             except jsonschema.exceptions.ValidationError as exc:
+#                 raise ValueError(
+#                     "Invalid Basket. "
+#                     "Supplement Schema does not match at: ", file
+#                 ) from exc
 
-            except jsonschema.exceptions.ValidationError:
-                raise ValueError(
-                    "Invalid Basket. "
-                    "Supplement Schema does not match at: ", file
-                )
+#             except json.decoder.JSONDecodeError as exc:
+#                 raise ValueError(
+#                     "Invalid Basket. "
+#                     "Supplement could not be loaded into json at: ", file
+#                 ) from exc
 
-            except json.decoder.JSONDecodeError:
-                raise ValueError(
-                    "Invalid Basket. "
-                    "Supplement could not be loaded into json at: ", file
-                )
+#         if file_name == 'basket_metadata.json':
+#             try:
+#                 data = json.load(file_system.open(file))
 
-        if file_name == 'basket_metadata.json':
-            try:
-                data = json.load(fs.open(file))
-
-            except json.decoder.JSONDecodeError:
-                raise ValueError(
-                    "Invalid Basket. "
-                    "Metadata could not be loaded into json at: ", file
-                )
+#             except json.decoder.JSONDecodeError as exc:
+#                 raise ValueError(
+#                     "Invalid Basket. "
+#                     "Metadata could not be loaded into json at: ", file
+#                 ) from exc
 
         # if we find a directory inside this basket, we need to check it
         # if we check it and find a basket, this basket is invalid.
-        if fs.info(file)['type'] == 'directory':
-            if _check_level(file, fs, in_basket=True):
-                raise ValueError("Invalid Basket. Manifest File "
-                                 "found in sub directory of basket at: ",
-                                 basket_dir
-                )
+        # if file_system.info(file)['type'] == 'directory':
+        #     if _check_level(file, file_system, in_basket=True):
+        #         raise ValueError("Invalid Basket. Manifest File "
+        #                          "found in sub directory of basket at: ",
+        #                          basket_dir
+        #         )
 
     # default return true if we don't find any problems with this basket
     return True
+
+
+def handle_manifest(file, file_system):
+    """Handles case if manifest"""
+    try:
+        # these two lines make sure it can be read and is valid schema
+        data = json.load(file_system.open(file))
+        validate(instance=data, schema=config.manifest_schema)
+
+    except jsonschema.exceptions.ValidationError as exc:
+        raise ValueError(
+            "Invalid Basket. Manifest Schema does not match at: ", file
+        ) from exc
+
+    except json.decoder.JSONDecodeError as exc:
+        raise ValueError(
+            "Invalid Basket. Manifest could not be loaded into json at: ", file
+        ) from exc
+
+
+def handle_supplement(file, file_system):
+    """Handles case if supplement"""
+    try:
+        # these two lines make sure it can be read and is valid schema
+        data = json.load(file_system.open(file))
+        validate(instance=data, schema=config.supplement_schema)
+
+    except jsonschema.exceptions.ValidationError as exc:
+        raise ValueError(
+            "Invalid Basket. "
+            "Supplement Schema does not match at: ", file
+        ) from exc
+
+    except json.decoder.JSONDecodeError as exc:
+        raise ValueError(
+            "Invalid Basket. "
+            "Supplement could not be loaded into json at: ", file
+        ) from exc
+
+def handle_metadata(file, file_system):
+    """Handles case if metadata"""
+    try:
+        json.load(file_system.open(file))
+
+    except json.decoder.JSONDecodeError as exc:
+        raise ValueError(
+            "Invalid Basket. "
+            "Metadata could not be loaded into json at: ", file
+        ) from exc
+
+
+def handle_none_of_the_above(file, file_system):
+    """Handles case if none of the above"""
+    basket_dir, _ = os.path.split(file)
+    if file_system.info(file)['type'] == 'directory':
+        if _check_level(file, file_system, in_basket=True):
+            raise ValueError("Invalid Basket. Manifest File "
+                             "found in sub directory of basket at: ",
+                             basket_dir
+            )
