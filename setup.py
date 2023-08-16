8519d3b52758c8561bc8442bdade128b623d9b05
Jonathan Adamic <jonathan.adamic@us.af.mil>
2023-08-16 15:56:16 +0000

    Version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="weave",
-    version="0.8.2",
+    version="0.9.1",
     packages=["weave", "weave/tests"],
     install_requires=["pandas", "s3fs", "fsspec", "pymongo", "jsonschema"]
 )
