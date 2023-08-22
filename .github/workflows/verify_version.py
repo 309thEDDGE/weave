b3ad533364dcfc0d660c4e294c8655696b023733
Braden Eichmeier <braden.eichmeier.1@us.af.mil>
2023-08-22 14:21:40 +0000

    Add verify_version.py

---
+++ .github/workflows/verify_version.py
@@ -0,0 +1,23 @@
+import sys
+
+def verify_version(old_vers, new_vers):
+    old_vers = [int(v) for v in old_vers.split('.')]
+    valids = [
+        [old_vers[0]+1, 0, 0],
+        [old_vers[0], old_vers[1]+1, 0],
+        [old_vers[0], old_vers[1], old_vers[2]+1],
+    ]
+
+    for i, valid in enumerate(valids):
+        valids[i] = ('.').join([str(v) for v in valids[i]])
+
+    if new_vers in valids:
+        return True
+    print(f"New version not valid. Valid version increments: {valids}")
+    return False
+
+if __name__=="__main__":
+    old_version = sys.argv[1]
+    new_version = sys.argv[2]
+
+    verify_version(old_version, new_version)
