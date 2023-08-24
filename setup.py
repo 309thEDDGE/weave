7e5f358fb641d9a2a0bb256891bf7b3879b4e1c2
CW-USAF <137530071+CW-USAF@users.noreply.github.com>
2023-08-24 13:21:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
-import os
 from setuptools import setup
-
-def read(rel_path: str) -> str:
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with open(os.path.join(here, rel_path)) as fp:
-        return fp.read()
-
-
-def get_version(rel_path: str) -> str:
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
-
 
 setup(
     name="weave",
-    version=get_version("weave/__init__.py"),
-    packages=["weave", "weave/tests", "weave/index"],
+    version="0.8.1",
+    packages=["weave", "weave/tests"],
     install_requires=["pandas", "s3fs", "fsspec", "pymongo", "jsonschema"]
 )
