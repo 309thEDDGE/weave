fcf93ebfb56602de1e886cd3e3a1a54e1d9c2d87
CW-USAF <137530071+CW-USAF@users.noreply.github.com>
2023-08-24 13:17:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="weave",
-    version="0.8.1",
+    version="0.10.7",
     packages=["weave", "weave/tests"],
     install_requires=["pandas", "s3fs", "fsspec", "pymongo", "jsonschema"]
 )
