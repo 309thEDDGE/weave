d10103bca3f855e550d0b26cfad01e5250e05112
GZ-USAF <grant.zelinka@us.af.mil>
2023-08-31 23:14:16 +0000

    fake baskets in fake pantrys

--- weave/tests/test_uploader.py
+++ weave/tests/test_uploader.py
@@ -575,9 +575,7 @@ def test_upload_basket_without_uuid_creates_uuid(test_basket):
         file_system=test_basket.file_system,
     )
     assert uploading_basket.kwargs.get("unique_id") is not None
-    print(test_basket.file_system)
-    print(type(test_basket.file_system))
-    print(test_basket.file_system.ls)
+    print(test_basket.file_system.ls('/'))
     assert uploaded_basket.get_manifest()['uuid'] != 'null'
 
 
