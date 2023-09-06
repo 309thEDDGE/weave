a0069437fe0382556f8af0c19c2c0327f33a0dad
GZ-USAF <grant.zelinka@us.af.mil>
2023-09-06 21:13:24 +0000

    file not found debugging

--- weave/tests/test_uploader.py
+++ weave/tests/test_uploader.py
@@ -554,13 +554,14 @@ def test_upload_basket_without_uuid_creates_uuid(test_basket):
     assert uploading_basket.kwargs.get("unique_id") is not None
 
     full_upload_path = os.path.join(os.getcwd(),upload_path)
-    manifest_path = os.path.join(full_upload_path,"basket_manifest.json")
-    
-    with open(manifest_path, "r", encoding="utf-8") as outfile:
+    current_working_directory = os.getcwd()
+    os.chdir(full_upload_path)
+
+    with open("basket_manifest.json", "r", encoding="utf-8") as outfile:
         manifest_data = json.load(outfile)
 
     assert manifest_data['uuid'] != 'null'
-
+    os.chdir(current_working_directory)
 
 def test_upload_basket_upload_items_is_not_a_string(test_basket):
     """
