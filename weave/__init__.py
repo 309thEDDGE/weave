049f7335310031414c8ecee0026e12b2d6d77fdc
Jonathan Adamic <jonathan.adamic@us.af.mil>
2023-09-21 21:54:19 +0000

    version number

--- weave/__init__.py
+++ weave/__init__.py
@@ -25,7 +25,7 @@ from .index.index_sqlite import IndexSQLite
 from .pantry import Pantry
 from .metadata_db import load_mongo
 
-__version__ = "0.14.2"
+__version__ = "0.14.3"
 
 __all__ = [
     "Basket",
