fcc5104b7b16148656612bb44614b2fb9af86aae
Jonathan Adamic <jonathan.adamic@us.af.mil>
2023-10-10 22:10:09 +0000

    updating sql queries

--- weave/tests/test_pytest.py
+++ weave/tests/test_pytest.py
@@ -84,17 +84,26 @@ def test_github_cicd_sql_server():
 
     print(cur.execute("SELECT @@version;").fetchone())
 
+    # Create a temporary table for testing.
     cur.execute("""
     IF NOT EXISTS (
         SELECT * FROM sys.tables t
         JOIN sys.schemas s ON (t.schema_id = s.schema_id)
-        WHERE s.name = 'dbo' AND t.name = 'test_table')
-        CREATE TABLE dbo.test_table (
+        WHERE s.name = 'pytest' AND t.name = 'test_table')
+        CREATE TABLE pytest.test_table (
             uuid varchar(64),
             num int
         );
     """)
 
-    cur.execute("""INSERT INTO dbo.test_table VALUES ("0001", 1);""")
+    # Insert a test value, and then check we can retrieve the value.
+    cur.execute("""INSERT INTO pytest.test_table (uuid, num) VALUES ("0001", 1);""")
     cur.commit()
-    print(cur.execute("SELECT * dbo.FROM test_table").fetchall())
+    assert cur.execute("SELECT * pytest.FROM test_table").fetchall() != []
+
+    # Delete the test value, and then check it was actually deleted.
+    cur.execute("""DELETE FROM dbo.test_table WHERE uuid = "0001";""")
+    cur.commit()
+    assert cur.execute("SELECT * pytest.FROM test_table;").fetchall() == []
+
+    cur.execute("""DROP TABLE test_table;""")
