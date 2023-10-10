574ea12283519bcd37aefb6a9a6d9cd993adad88
CW-USAF <carson.wilkins@us.af.mil>
2023-10-10 16:21:55 +0000

    Update tests

--- weave/tests/test_index.py
+++ weave/tests/test_index.py
@@ -978,11 +978,10 @@ def test_index_abc_get_parents_max_gen_level(test_pantry):
     ind.generate_index()
 
     parents_df = ind.get_parents("0000", max_gen_level=2)
-    # Create a dataframe with max_gen_level filtered. This should be exactly
-    # equal to the parents_df if max_gen_level works correctly.
-    max_gen_level_parents_df = parents_df[parents_df["generation_level"] <= 2]
+    parents_df_uuids = parents_df["uuid"].tolist()
 
-    assert parents_df.equals(max_gen_level_parents_df)
+    # Check that max_gen_level filters out the correct uuids
+    assert parents_df_uuids == ["1000", "1001", "2000", "2002", "3303"]
 
 
 def test_index_abc_get_children_path_works(test_pantry):
@@ -1359,12 +1358,10 @@ def test_index_abc_get_children_min_gen_level(test_pantry):
     ind.generate_index()
 
     children_df = ind.get_children("3000", min_gen_level=-2)
-    # Create a dataframe with min_gen_level filtered. This should be exactly
-    # equal to the children_df if min_gen_level works correctly.
-    min_gen_level_children_df = children_df[
-                                children_df["generation_level"] >= -2]
+    children_df_uuids = children_df["uuid"].tolist()
 
-    assert children_df.equals(min_gen_level_children_df)
+    # Check that min_gen_level filters the correct uuids
+    assert children_df_uuids == ["2000", "1000"]
 
 
 def test_index_abc_get_baskets_of_type_works(test_pantry):
