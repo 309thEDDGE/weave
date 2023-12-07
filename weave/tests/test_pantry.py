764a2d9422a6712b9e0cd16292502717333298b6
AB-USAF <alexander.bedoya@us.af.mil>
2023-12-07 23:06:28 +0000

    fixed read-only fs bug

--- weave/tests/test_pantry.py
+++ weave/tests/test_pantry.py
@@ -715,7 +715,7 @@ def test_upload_basket_read_only():
                                        "zip",
                                        tmpdir)
 
-        read_only_fs = fsspec.filesystem("zip", fo=zip_path)
+        read_only_fs = fsspec.filesystem("zip", fo=zip_path, mode="r")
         read_only_pantry = Pantry(IndexPandas,
                                   pantry_path="",
                                   file_system=read_only_fs)
