47628c6b16350c2d208ceb732e8ae268f1cc8392
AB-USAF <alexander.bedoya@us.af.mil>
2023-12-08 18:38:04 +0000

    more testing

--- weave/tests/test_index_sql.py
+++ weave/tests/test_index_sql.py
@@ -95,7 +95,7 @@ def test_index_sql_properties_are_read_only():
     are read only, and cannot be changed during execution.
     """
     pantry_path = (
-        "weave-pytest-temp-pantry"
+        "pytest-temp-pantry"
         f"{os.environ.get('WEAVE_PYTEST_SUFFIX', '')}"
     )
 
