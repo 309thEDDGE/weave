0e8c13ed411104dfe5fa5fcbee191c0d6ced9316
pat <quoc-anh,nguyen@us.af.mil>
2024-05-28 18:08:12 +0000

    update

--- weave/tests/test_mongo_db.py
+++ weave/tests/test_mongo_db.py
@@ -135,7 +135,9 @@ def test_load_mongo_metadata(set_up):
     for item in db_data:
         item.pop("_id")
         compared_data.append(item)
-    assert truth_db == compared_data
+
+    for e in truth_db:
+        assert e in compared_data
 
 
 @pytest.mark.skipif(
