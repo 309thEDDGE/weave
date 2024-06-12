74bde95582223a22f09eec0c085ae0ec89a0451e
pat <quoc-anh,nguyen@us.af.mil>
2024-06-12 16:32:37 +0000

    fixed ruff

--- weave/pantry.py
+++ weave/pantry.py
@@ -35,7 +35,8 @@ class Pantry():
     """Facilitate user interaction with the index of a Weave data warehouse.
     """
 
-    def __init__(self, index: IndexABC, pantry_path="weave-test", use_mongo=False, **kwargs):
+    def __init__(self, index: IndexABC, pantry_path="weave-test",
+                 use_mongo=False, **kwargs):
         """Initialize Pantry object.
 
         A pantry is a collection of baskets. This class facilitates the upload,
