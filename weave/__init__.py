5210589c5114253ce2fa5a1809a1129346a6313b
pat <quoc-anh,nguyen@us.af.mil>
2024-06-26 16:21:28 +0000

    added imports

--- weave/__init__.py
+++ weave/__init__.py
@@ -24,6 +24,7 @@ from .index.index_pandas import IndexPandas
 from .index.index_sqlite import IndexSQLite
 from .index.index_sql import IndexSQL
 from .pantry import Pantry
+from .pantry_factory import create_pantry
 from .mongo_loader import MongoLoader
 
 __version__ = "1.9.2"
