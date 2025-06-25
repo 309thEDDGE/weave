5b905ed774afbe6b37d8b540687944ff6082632e
carson.wilkins <carson.wilkins@us.af.mil>
2025-06-25 14:11:45 -0600

    pylint fix

--- weave/basket.py
+++ weave/basket.py
@@ -362,7 +362,8 @@ def create_basket_in_place(directory_path, **kwargs):
     # Create manifest file
     manifest = {
         "uuid": str(uuid.uuid1().hex),
-        "upload_time": datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        "upload_time": datetime.datetime.now(
+                            datetime.timezone.utc).isoformat(),
         "parent_uuids": parent_uuids or [],
         "basket_type": basket_type,
         "label": label,
