2fabf56455058930e67fab4f21491e3af44fcf35
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-07-09 11:42:03 -0600

    Added a list of basked metadata to be returned

--- weave/analytics/dummy_baskets.py
+++ weave/analytics/dummy_baskets.py
@@ -9,7 +9,7 @@ from fsspec.implementations.local import LocalFileSystem
 
 
 def generate_dummy_baskets(index_type, file_name="dummy_data.txt", basket_count=1000, print_time=True):
-    """Generates a 1MB file of dummy data and uploads it to a pantry as 10 baskets.
+    """Generates a 1MB file of dummy data and uploads it to a pantry as a basket count.
     Used to test the time it takes to handle large files in the baskets.
 
     Parameters:
@@ -25,6 +25,9 @@ def generate_dummy_baskets(index_type, file_name="dummy_data.txt", basket_count=
     size_in_bytes = 1024 * 1024
     chunk_size = 1024  # 1 KB chunks
     characters = string.ascii_letters + string.digits
+    
+    #List of metadata about the baskets
+    basket_metadata = []
 
     with open(file_name, "w") as f:
         for _ in range(size_in_bytes // chunk_size):
@@ -43,21 +46,39 @@ def generate_dummy_baskets(index_type, file_name="dummy_data.txt", basket_count=
     #Generate 10 baskets containing the 1MB file dummy_data.txt
     for uuid in range(basket_count):
         uuid = str(uuid)
+        
         dummy_pantry.upload_basket(upload_items=[{'path':file_name, 'stub':False}], basket_type="dummy_baskets", metadata = {'Data Type':'text'})
+        
+        
+        basket_metadata.append({
+            'path': file_name,
+            'basket_type': 'dummy_baskets',
+            'metadata': {'Data Type': 'text'},
+         })
+
     
     if print_time:
         end_time = time.time()
     
         time_elapsed = end_time - start_time
         print(f"Time taken to generate {basket_count} dummy baskets for {index_type.__name__}: {time_elapsed} seconds")
-
+    breakpoint()
     #Cleanup the pantry
     shutil.rmtree(dummy_pantry.pantry_path)
     
     #Cleanup the .db file if using SQLite index
     if(index_type == IndexSQLite):
         dummy_pantry.index.drop_index()
-    
+
     #Delete the dummy txt if it exists
     if os.path.exists(file_name):
         os.remove(file_name)
+
+    #TODO: return a list of everything you need to know to upload one basket
+    #return 2 lists 1st list path and basket type
+    #second list metadata
+    #print( {"basket_count":basket_count, "size_of_file":size_in_bytes, "basket_path": "metadata":list_of_metadata} )
+
+    return basket_metadata
+    
+
