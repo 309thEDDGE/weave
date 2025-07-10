9ed2e5b824b048642c8d49c401026c741ed1a84c
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-07-10 14:54:19 -0600

    Updated dummy_files.py to create multiple baskets

--- weave/analytics/dummy_files.py
+++ weave/analytics/dummy_files.py
@@ -5,36 +5,43 @@ import random
 import time
 import json
 from datetime import datetime, timedelta
+from weave.pantry import Pantry
 from weave.basket import Basket
+from weave.index.index_pandas import IndexPandas
 from weave.index.index_sqlite import IndexSQLite
 from fsspec.implementations.local import LocalFileSystem
 
-def generate_dummy_files(file_count=1000, file_size_mb=1, file_path="weave/analytics/dummy"):
+def generate_dummy_files(basket_count=1000, file_count=10, file_size_mb=1, file_path="weave/analytics/dummy"):
     """Generates dummy files in the specified directory with random text content.
 
         Parameters:
         -----------
-        file_path: str
-            Defaults to "weave/analytics".  Specifies the directory where dummy files will be created.
+        basket_count: int
+            Defaults to 1000.
+            Specifies the number of dummy baskets to create.
         file_count: int
             Defaults to 10.
             Specifies the number of dummy files to create.
         file_size_mb: int
             Defaults to 1.
-            Specifies the size of each dummy file in megabytes. 
+            Specifies the size of each dummy file in megabytes.
+        file_path: str
+            Defaults to "weave/analytics/dummy".
+            Specifies the path where the dummy files will be created.
     """
-    metadata_list = list()
-
-    # Create the directories
+    #List of all baskets created
+    basket_list = []
+    
+    # Create the dummy files using the file count, size, and path provided
     for i in range(file_count):
-        dir_path = os.path.join(file_path, f"dummy_folder_{i}")
+        dir_path = os.path.join(file_path)
         os.makedirs(dir_path, exist_ok=True)
 
         # Create a dummy file of specified size
         size_in_bytes = file_size_mb * 1024 * 1024
         chunk_size = 1024
         
-        # Generate random text and write to the file
+        # Generate metadata for the dummy file
         with open(os.path.join(dir_path, f"dummy_file_{i}.txt"), "w") as f:
             for _ in range(size_in_bytes // chunk_size):
                 flight_number = f"{random.choice(['UA', 'DL', 'AA', 'SW'])}{random.randint(100,9999)}"
@@ -59,16 +66,13 @@ def generate_dummy_files(file_count=1000, file_size_mb=1, file_path="weave/analy
                     "speed": speed,
                     "status": status
                 }
-                
-                metadata_list.append(metadata)
-                
+                                
                 json.dump(metadata, f)
-                f.write("\n")
+                
+    # Add a basket containing the dummy files to the basket list
+    for _ in range(basket_count):
+        basket_list.append({"upload_items": [{'path' :file_path, 'stub': False}], "basket_type": "dummy-baskets", "metadata": {'Data Type': 'text'}})
 
-    # Clean up the created directories and files
-    if os.path.exists(file_path):
-        shutil.rmtree(file_path)
-    return {"file_count":file_count, "file_size":file_size_mb, "file_location":file_path, "dummy metadata":metadata_list}
+    # Return the basket list
+    return basket_list
     
-# print(generate_dummy_files())
-generate_dummy_files()
