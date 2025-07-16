264eba77da28b7a5c75ce51e362266cb4f79b12b
JonAdamic <36721782+JonAdamic@users.noreply.github.com>
2025-07-16 11:23:33 -0600

    Fix absolute pantry paths (#144)
    * Fixed a bug where using absolute pantry paths caused several errors.
    
    * Fix line length limits, and rename basket_json vars to be more accurate
    
    * Replace windows backslashes with forward

--- weave/index/create_index.py
+++ weave/index/create_index.py
@@ -39,7 +39,7 @@ def create_index_from_fs(root_dir, file_system):
     if not file_system.exists(root_dir):
         raise FileNotFoundError(f"'root_dir' does not exist '{root_dir}'")
 
-    basket_jsons = _get_list_of_basket_jsons(root_dir, file_system)
+    manifest_paths = _get_list_of_basket_jsons(root_dir, file_system)
     index_columns = get_index_column_names()
     index_dict = {}
 
@@ -47,31 +47,36 @@ def create_index_from_fs(root_dir, file_system):
         index_dict[key] = []
 
     bad_baskets = []
-    for basket_json_address in basket_jsons:
-        with file_system.open(basket_json_address, "rb") as file:
+    for manifest_path in manifest_paths:
+        with file_system.open(manifest_path, "rb") as file:
             basket_dict = json.load(file)
             if not validate_basket_dict(basket_dict):
-                bad_baskets.append(os.path.dirname(basket_json_address))
+                bad_baskets.append(os.path.dirname(manifest_path))
                 continue
             basket_dict["upload_time"] = pd.Timestamp(
                                                 basket_dict["upload_time"]
                                          )
-            if basket_dict["basket_type"] != "index":
-                for field in basket_dict.keys():
-                    index_dict[field].append(basket_dict[field])
+            if basket_dict["basket_type"] == "index":
+                continue
 
-                index_dict["address"].append(
-                    os.path.relpath(os.path.dirname(basket_json_address))
-                )
-                index_dict["storage_type"].append(
-                    file_system.__class__.__name__
-                )
+            for field in basket_dict.keys():
+                index_dict[field].append(basket_dict[field])
 
-                if "weave_version" not in basket_dict.keys():
-                    # Every basket uploaded before 0.13.0, should not have a
-                    # version number, therefore every basket with no version
-                    # number will be shown as <0.13.0
-                    index_dict["weave_version"].append("<0.13.0")
+            if manifest_path.startswith('/'):
+                address = os.path.normpath(os.path.dirname(manifest_path))
+            else:
+                address = os.path.relpath(os.path.dirname(manifest_path))
+            index_dict["address"].append(address)
+
+            index_dict["storage_type"].append(
+                file_system.__class__.__name__
+            )
+
+            if "weave_version" not in basket_dict.keys():
+                # Every basket uploaded before 0.13.0, should not have a
+                # version number, therefore every basket with no version
+                # number will be shown as <0.13.0
+                index_dict["weave_version"].append("<0.13.0")
 
     if len(bad_baskets) != 0:
         warnings.warn("baskets found in the following locations "
