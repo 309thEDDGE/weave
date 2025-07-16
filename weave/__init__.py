d705488d853e551424fbe43de096a2a9ac0e63e9
Anastasia <anastasia.slzr00@gmail.com>
2025-07-16 10:11:57 -0600

    version check

--- weave/__init__.py
+++ weave/__init__.py
@@ -27,7 +27,7 @@ from .pantry import Pantry
 from .pantry_factory import create_pantry
 from .mongo_loader import MongoLoader
 
-__version__ = "1.13.10"
+__version__ = "1.14.1"
 
 __all__ = [
     "Basket",
