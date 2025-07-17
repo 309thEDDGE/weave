aa51aa3bcf4b6de111dd46648db7e6a8ad2ec28e
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-07-17 14:27:54 -0600

    fixed line spacing

--- weave/analytics/dummy_baskets.py
+++ weave/analytics/dummy_baskets.py
@@ -5,9 +5,11 @@ import uuid
 from pathlib import Path
 from datetime import datetime, timedelta
 
-def generate_dummy_baskets(basket_count=1000, file_count=10, file_size_mb=1, file_path="dummy_data", num_basket_types=5):
-    """Generates dummy files in the specified directory with random text content.
-        Each basket will contain the same list of dummy files from the dummy_data directory.
+def generate_dummy_baskets(basket_count=1000, file_count=10, file_size_mb=1, 
+    file_path="dummy_data", num_basket_types=5):
+    """Generates dummy files in the specified directory with 
+        random text content. Each basket will contain the same list of 
+        dummy files from the dummy_data directory.
 
         Parameters:
         -----------
@@ -44,16 +46,29 @@ def generate_dummy_baskets(basket_count=1000, file_count=10, file_size_mb=1, fil
             # Generate data for the dummy file
             with open(os.path.join(dir_path, f"dummy_file_{i}.txt"), "w") as f:
                 for _ in range(int((size_in_bytes // chunk_size))):
-                    flight_number = f"{random.choice(['UA', 'DL', 'AA', 'SW'])}{random.randint(100,9999)}"
+                    airline = random.choice(['UA', 'DL', 'AA', 'SW'])
+                    flight_num = random.randint(100, 9999)
+                    flight_number = f"{airline}{flight_num}"
                     now = datetime.now()
                     departure_time = now.strftime("%Y-%m-%d %H:%M")
-                    arrival_time = (now + timedelta(hours=random.randint(1, 12))).strftime("%Y-%m-%d %H:%M")
-                    origin = random.choice(['JFK', 'LAX', 'ORD', 'ATL', 'DFW'])
-                    destination = random.choice(['SEA', 'MIA', 'DEN', 'PHX', 'SFO'])
-                    aircraft_type = random.choice(['A320', 'B737', 'B777', 'A380'])
+                    hours = random.randint(1, 12)
+                    arrival_time = (
+                        now + timedelta(hours=hours)
+                    ).strftime("%Y-%m-%d %H:%M")
+                    origin = random.choice([
+                        'JFK', 'LAX', 'ORD', 'ATL', 'DFW'
+                    ])
+                    destination = random.choice([
+                        'SEA', 'MIA', 'DEN', 'PHX', 'SFO'
+                    ])
+                    aircraft_type = random.choice([
+                        'A320', 'B737', 'B777', 'A380'
+                    ])
                     altitude = random.randint(30000, 41000)  # feet
                     speed = random.randint(400, 600)  # knots
-                    status = random.choice(['Scheduled', 'Departed', 'Arrived', 'Delayed'])
+                    status = random.choice([
+                        'Scheduled', 'Departed', 'Arrived', 'Delayed'
+                    ])
 
                     data = {
                         "flight_number": flight_number,
@@ -66,14 +81,15 @@ def generate_dummy_baskets(basket_count=1000, file_count=10, file_size_mb=1, fil
                         "speed": speed,
                         "status": status
                     }
-                                    
                     json.dump(data, f)
                     
     # Add a basket containing the dummy files to the basket list
     counter = 0
     x = 1
     for _ in range(basket_count):
-        basket_list.append({"upload_items": [{'path' :file_path, 'stub': False}], "basket_type": f"dummy-baskets-{x}", "metadata": {'Data Type': 'text'}})
+        basket_list.append({"upload_items": [{'path' :file_path, 'stub': False}
+            ], "basket_type": f"dummy-baskets-{x}", 
+            "metadata": {'Data Type': 'text'}})
 
         if(num_basket_types > 0):
             counter += 1
--- weave/tests/test_analytics.py
+++ weave/tests/test_analytics.py
@@ -14,7 +14,8 @@ import pytest
 from pathlib import Path
 
 def cleanup_test_directory():
-    """Cleanup the test directory by removing the dummy data and pantry directories.
+    """Cleanup the test directory by removing the dummy data 
+    and pantry directories.
     And remove the dummy_pantry.db file if it exists."""
     if os.path.exists("weave/analytics/test_dummy_data"):
         shutil.rmtree("weave/analytics/test_dummy_data")
@@ -28,76 +29,92 @@ def cleanup_test_directory():
             pass
 
 def test_dummy_baskets_basket_count():
-    """Test the generate_dummy_baskets function to ensure it creates the expected number of baskets and files.
+    """Test the generate_dummy_baskets function to ensure it creates the 
+    expected number of baskets and files. 
     Expected: 10 baskets with 5 files each"""
+    
     file_path = "weave/analytics/test_dummy_data"
     pantry_path = "weave/analytics/dummy_pantry"
-    baskets = generate_dummy_baskets(basket_count=10, file_count=5, file_size_mb=1, file_path=str(file_path), num_basket_types=3)
+    baskets = generate_dummy_baskets(basket_count=10, file_count=5, 
+        file_size_mb=1, file_path=str(file_path), num_basket_types=3)
     
-    assert len(baskets) == 10, "Ensure baskets returns a list of data to make 10 baskets"
+    assert len(baskets) == 10
 
     #Create a new pantry
     local_fs = LocalFileSystem()
-    test_pantry = Pantry(IndexSQLite, pantry_path=str(pantry_path), file_system=local_fs)
+    test_pantry = Pantry(IndexSQLite, pantry_path=str(pantry_path), 
+                         file_system=local_fs)
 
     #Use ** to unpack the dictionary returned by generate_dummy_files
     for basket in baskets:
         test_pantry.upload_basket(**basket)
     
-    files = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
-    assert len(files) == 5, "Ensure 5 files are created in the dummy data directory"
+    files = [f for f in os.listdir(file_path) if 
+             os.path.isfile(os.path.join(file_path, f))]
+    
+    assert len(files) == 5
     
     cleanup_test_directory()
 
 def test_dummy_baskets_empty_pantry():
-    """Test the generate_dummy_baskets function with no baskets to ensure it handles empty cases correctly"""
+    """Test the generate_dummy_baskets function with no baskets to ensure it 
+    handles empty cases correctly"""
     file_path = "weave/analytics/test_dummy_data"
-    baskets = generate_dummy_baskets(basket_count=0, file_count=5, file_size_mb=1, file_path=str(file_path), num_basket_types=3)
+    baskets = generate_dummy_baskets(basket_count=0, file_count=5, 
+        file_size_mb=1, file_path=str(file_path), num_basket_types=3)
     
-    assert len(baskets) == 0, "Ensure baskets returns a list of data to make 10 baskets"
+    assert len(baskets) == 0
 
     #Create a new pantry
     local_fs = LocalFileSystem()
-    test_pantry = Pantry(IndexSQLite, pantry_path=str("weave/analytics/dummy_pantry"), file_system=local_fs)
+    test_pantry = Pantry(IndexSQLite,
+        pantry_path=str("weave/analytics/dummy_pantry"), file_system=local_fs)
 
     #Use ** to unpack the dictionary returned by generate_dummy_files
     for basket in baskets:
         test_pantry.upload_basket(**basket)
     
-    files = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
-    assert len(files) == 5, "Ensure 5 files are created in the dummy data directory"
+    files = [f for f in os.listdir(file_path) 
+        if os.path.isfile(os.path.join(file_path, f))]
+    assert len(files) == 5
     cleanup_test_directory()
     
     
 def test_dummy_baskets_no_files():
-    """Test the generate_dummy_baskets function with no files to ensure it handles empty cases correctly"""
+    """Test the generate_dummy_baskets function 
+    with no files to ensure it handles empty cases correctly"""
     file_path = "weave/analytics/test_dummy_data"
-    baskets = generate_dummy_baskets(basket_count=10, file_count=0, file_size_mb=1, file_path=str(file_path), num_basket_types=3)
+    baskets = generate_dummy_baskets(basket_count=10, file_count=0, 
+        file_size_mb=1, file_path=str(file_path), num_basket_types=3)
     
     assert len(baskets) == 10
     
     if os.path.exists(file_path):
         # Ensure no baskets are created when file_count is 0
-        assert False, "Expected no files to be created when file_count is 0"
+        assert False
     else:
-        assert True, "Dummy data directory does not exist, as expected when file_count is 0"
+        assert True
     
     cleanup_test_directory()
 
 def test_dummy_baskets_empty_files():
-    """Test the generate_dummy_baskets function with empty files to ensure the correct number of empty files are generated"""
+    """Test the generate_dummy_baskets function with empty files to
+    ensure the correct number of empty files are generated"""
     file_path = "weave/analytics/test_dummy_data"
-    baskets = generate_dummy_baskets(basket_count=10, file_count=5, file_size_mb=0, file_path=str(file_path), num_basket_types=3)
+    baskets = generate_dummy_baskets(basket_count=10, file_count=5,
+        file_size_mb=0, file_path=str(file_path), num_basket_types=3)
     
       #Create a new pantry
     local_fs = LocalFileSystem()
-    test_pantry = Pantry(IndexSQLite, pantry_path=str("weave/analytics/dummy_pantry"), file_system=local_fs)
+    test_pantry = Pantry(IndexSQLite, 
+        pantry_path=str("weave/analytics/dummy_pantry"), file_system=local_fs)
 
     #Use ** to unpack the dictionary returned by generate_dummy_files
     for basket in baskets:
         test_pantry.upload_basket(**basket)
     
-    files = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
+    files = [f for f in os.listdir(file_path)
+        if os.path.isfile(os.path.join(file_path, f))]
     assert len(files) == 5, "Ensure the 5 files are created"
     
     # Check if the files are empty
@@ -112,30 +129,35 @@ def test_dummy_baskets_no_basket_types():
     """Test the generate_dummy_baskets function with no basket types"""
     file_path = "weave/analytics/test_dummy_data"
     pantry_path = "weave/analytics/dummy_pantry"
-    baskets = generate_dummy_baskets(basket_count=10, file_count=5, file_size_mb=1, file_path=str(file_path), num_basket_types=0)
+    baskets = generate_dummy_baskets(basket_count=10, file_count=5,
+        file_size_mb=1, file_path=str(file_path), num_basket_types=0)
     
-    assert len(baskets) == 10, "Ensure baskets returns a list of data to make 10 baskets"
+    assert len(baskets) == 10
 
     #Create a new pantry
     local_fs = LocalFileSystem()
-    test_pantry = Pantry(IndexSQLite, pantry_path=str(pantry_path), file_system=local_fs)
+    test_pantry = Pantry(IndexSQLite, 
+        pantry_path=str(pantry_path), file_system=local_fs)
 
     #Use ** to unpack the dictionary returned by generate_dummy_files
     for basket in baskets:
         test_pantry.upload_basket(**basket)
     
     # make sure there is only one directory created in dummy_pantry
-    directories = [d for d in os.listdir(pantry_path) if os.path.isdir(os.path.join(pantry_path, d))]
-    assert len(directories) == 1, "Ensure only one directory for baskets is created in the pantry"
+    directories = [d for d in os.listdir(pantry_path)
+        if os.path.isdir(os.path.join(pantry_path, d))]
+    assert len(directories) == 1
     cleanup_test_directory()
 
     
 def test_dummy_baskets_negative_values():
-    """Test the generate_dummy_baskets function with negative values to ensure it handles them correctly"""
+    """Test the generate_dummy_baskets function with negative values
+        to ensure it handles them correctly"""
     file_path = "weave/analytics/test_dummy_data"
-    baskets = generate_dummy_baskets(basket_count=-10, file_count=-5, file_size_mb=-1, file_path=str(file_path), num_basket_types=3)
+    baskets = generate_dummy_baskets(basket_count=-10, file_count=-5, 
+        file_size_mb=-1, file_path=str(file_path), num_basket_types=3)
     
-    assert len(baskets) == 0, "Ensure baskets returns an empty list when negative values are provided"
+    assert len(baskets) == 0
     
     if os.path.exists(file_path):
         assert False
@@ -145,9 +167,10 @@ def test_dummy_baskets_negative_values():
     cleanup_test_directory()
     
 def test_dummy_baskets_non_string_filepath():
-    """Test the generate_dummy_baskets function with negative values to ensure it handles them correctly"""
+    """Test the generate_dummy_baskets function with a non-string file_path"""
     file_path = 123
-    baskets = generate_dummy_baskets(basket_count=10, file_count=5, file_size_mb=1, file_path=file_path, num_basket_types=3)
+    baskets = generate_dummy_baskets(basket_count=10, file_count=5,
+        file_size_mb=1, file_path=file_path, num_basket_types=3)
     
     if (os.path.exists(file_path)):
         assert False
