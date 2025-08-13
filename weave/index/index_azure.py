2135e2041a6708c2be368a9b614210d558b2fc34
carson.wilkins <carson.wilkins@us.af.mil>
2025-08-13 09:50:10 -0600

    init

--- weave/index/index_azure.py
+++ weave/index/index_azure.py
@@ -1,14 +1,18 @@
 from azure.storage.blob import BlobClient, BlobServiceClient
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-conn_str = os.environ["AZURE_CONNECTION_STRING"]
-service_client = BlobServiceClient.from_connection_string(conn_str)
+acc_url = os.getenv("AZURE_ACCOUNT_URL")
+acc_key = os.getenv("AZURE_ACCOUNT_KEY")
+service_client = BlobServiceClient(account_url=acc_url, credential=acc_key)
 
 # List containers
 for container in service_client.list_containers():
     print(container['name'])
 
-con_client = service_client.get_container_client("index")
-# container_client = service_client.create_container("index")
-con_client.delete_container()
+# con_client = service_client.get_container_client("index")
+container_client = service_client.create_container("index")
+
+container_client.delete_container()
