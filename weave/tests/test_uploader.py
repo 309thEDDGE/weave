1b0ad755e215b32c8cf05f3075fe0910841bce4a
Douglas Fenwick <douglas.fenwick>
2026-08-31 17:18:37 -0600

    Version bump and ruff fixes

--- weave/tests/test_uploader.py
+++ weave/tests/test_uploader.py
@@ -407,9 +407,9 @@ def test_derive_integrity_data_small_byte_count(tmp_path):
     test_file = tmp_path / text_file_name
     test_file.write_text(text_file_content)
 
-    # Expected sha256 hash of the string "0123456789". This string is used as the
-    # file size is > 3*byte_count. So checksum is generated using bytes from
-    # beginning, middle, and end (instead of whole file content).
+    # Expected blake hash of the string "0123456789". This string is used as
+    # the file size is > 3*byte_count. So checksum is generated using bytes
+    # from beginning, middle, and end (instead of whole file content).
     e_hash = "4169cdc647f6a08fa4faf0cb731a18a1be41a82813f6261a810be698caddb359"
     assert e_hash == derive_integrity_data(str(test_file), 2)["hash"]
 
